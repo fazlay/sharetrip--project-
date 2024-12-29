7e977020a948763552deaf2602d7e11237e6f5d7
fazlay <rfazlay21@gmail.com>
2024-12-29 19:12:40 +0600

    remove console log and commented-out code from addToCart function

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -5,10 +5,7 @@ import { Product } from '@/types/product.types';
 const useCart = () => {
     const [cart, setCart] = useState<Product[]>([]);
     const addToCart = (product: Product) => {
-        console.log('product', product);
-        // if (!cart.find((item) => item.id === product.id)) {
         setCart([...cart, product]);
-        // }
     };
     const removeFromCart = (productId: number) => {
         setCart(cart.filter((item) => item.id !== productId));
